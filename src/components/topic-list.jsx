43bbff661e4bebee944a9cc767fb7c28547c8506
Joey Nguyen <jnn193@gmail.com>
2015-10-25 02:41:52 -0500

    Refactored code to use Reflux library for fetching and storing data.

--- src/components/topic-list.jsx
+++ src/components/topic-list.jsx
@@ -1,5 +1,5 @@
 var React = require('react');
-var Api = require('../utils/api');
+var TopicStore = require('../stores/topic-store');
 
 module.exports = React.createClass({
     getInitialState: function() {
@@ -8,11 +8,11 @@ module.exports = React.createClass({
         }
     },
     componentWillMount: function() {
-        Api.get('topics/defaults')
-            .then(function(data) {
-                this.setState({
-                    topics: data.data
-                });
+        TopicStore.getTopics()
+            .then(function() {
+                // We have successfully fetched topics
+                // topics are available TopicStore.topics
+                this.setState({topics: TopicStore.topics})
             }.bind(this));
     },
     render: function() {
