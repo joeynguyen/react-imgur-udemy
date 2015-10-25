2c34683905b8f75250f3efe5a1633ded9082644d
Joey Nguyen <jnn193@gmail.com>
2015-10-25 03:18:10 -0500

    Refactored code to listen to changes from the store instead of inside the component.

--- src/components/topic-list.jsx
+++ src/components/topic-list.jsx
@@ -1,19 +1,18 @@
 var React = require('react');
+var Reflux = require('reflux');
 var TopicStore = require('../stores/topic-store');
 
 module.exports = React.createClass({
+    mixins: [
+        Reflux.listenTo(TopicStore, 'onChange')
+    ],
     getInitialState: function() {
         return {
             topics: []
         }
     },
     componentWillMount: function() {
-        TopicStore.getTopics()
-            .then(function() {
-                // We have successfully fetched topics
-                // topics are available TopicStore.topics
-                this.setState({topics: TopicStore.topics})
-            }.bind(this));
+        TopicStore.getTopics();
     },
     render: function() {
         return <div className="list-group">
@@ -27,5 +26,8 @@ module.exports = React.createClass({
         return this.state.topics.map(function(topic) {
             return <li>{topic.name}</li>
         });
+    },
+    onChange: function(event, topics) {
+        this.setState({topics: topics});
     }
 });
